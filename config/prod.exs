25d5e32ea42c3917948218c3ea844534ec1fcb76
EQ-254 <eq-254@MacBook-Pro-EQ-254.local>
2021-06-17 15:37:38 +0700

    update

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :crawler_example, CrawlerExampleWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "quiet-retreat-66732.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "intense-spire-82004.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
